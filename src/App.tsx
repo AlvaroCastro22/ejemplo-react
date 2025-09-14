c118f919f80f1fde64589a7e22c0a22a6aba506e
AlvaroCastro22 <106389791+AlvaroCastro22@users.noreply.github.com>
2025-09-14 02:54:20 +0000

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       />
       
       <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colecciónxd</h2>
+          <h2 className="text-center">Nuestra Colección!!</h2>
 
           <div className="row mt-5">
               {state.data.map((guitar) => (
